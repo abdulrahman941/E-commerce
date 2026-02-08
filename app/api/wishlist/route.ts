35d7f6141c2bdaf2f16475b94f9b260d4bafe257
abdulrahman941 <abdulrahman.1999hemida@gmail.com>
2026-02-08 14:49:38 +0200

    fix: sync all file names and casing with local machine

--- app/api/wishlist/route.ts
+++ app/api/wishlist/route.ts
@@ -6,7 +6,7 @@ export async function GET(req:NextRequest){
     if(!token){
         return NextResponse.json({error:'unauthorized',status:401})
     }
-    const res=await fetch(`https://e-commerce-theta-weld-91.vercel.app/wishlist`,{
+    const res=await fetch(`https://ecommerce.routemisr.com/api/v1/wishlist`,{
         headers:{
         token:token.token,
         'content-Type':'application/json'
