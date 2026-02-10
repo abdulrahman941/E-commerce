a1a91bca263aaf574da9aabe1cb7a80f49b30d5a
abdulrahman941 <abdulrahman.1999hemida@gmail.com>
2026-02-10 11:46:02 +0200

    fix: sync all file names and casing with local machine

--- schema/access-token.ts
+++ schema/access-token.ts
@@ -2,7 +2,7 @@ import { decode } from "next-auth/jwt"
 import { cookies } from "next/headers"
 
 export async function getAccessToken(){
-    const authToken =((await cookies()).get('next-auth.session-token')?.value)
+    const authToken =((await cookies()).get('Secure-next-auth.session-token')?.value)
     const token= await decode({
         token:authToken,
         secret:"izdbM4ppU68hTKTzrC/aJRPZiBcmOio67tBEM3bN0OM="!
