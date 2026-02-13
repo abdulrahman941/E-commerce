d7178353d16e0ef12f2fffe6369bd65f21dcedca
abdulrahman941 <abdulrahman.1999hemida@gmail.com>
2026-02-13 13:04:20 +0200

    fix: sync all file names and casing with local machine

--- my-heroui-app/app1/layout.tsx
+++ my-heroui-app/app1/layout.tsx
@@ -5,9 +5,9 @@ import clsx from "clsx";
 
 import { Providers } from "./providers";
 
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
+import { siteConfig } from "../config/site";
+import { fontSans } from "../config/fonts";
+import { Navbar } from "../components/navbar";
 
 export const metadata: Metadata = {
   title: {
